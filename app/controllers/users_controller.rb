f11113e99819a6043974b718b955a5698b6a22f3
Alyssa Biot <alyssa.biot@gmail.com>
2017-01-11 13:12:04 +0100

    create and show user

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,28 @@
 class UsersController < ApplicationController
+  before_action :set_user, only: [ :show, :edit, :update, :destroy ]
+
+  def show
+  end
+
+  def new
+    @user = User.new()
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      redirect_to user_path(@user)
+    else
+      render :new
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:email, :last_name, :first_name)
+  end
+
+  def set_user
+    @user = User.find(params[:id])
+  end
 end
