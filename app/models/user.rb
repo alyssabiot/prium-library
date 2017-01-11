2e55ebc862841db9053390dde44672be2b6d7f88
Alyssa Biot <alyssa.biot@gmail.com>
2017-01-11 15:51:47 +0100

    menu and user index

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,6 @@ class User < ApplicationRecord
   validates :email, presence: true, uniqueness: true
   validates :last_name, presence: true
   validates :first_name, presence: true
-  has_many :books
   has_many :bookings
+  has_many :books, through: :bookings
 end
